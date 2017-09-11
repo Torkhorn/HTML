1428954185eb9585304dea9ae6feb23d6b0e1414
math <master_of_puppets57@hotmail.fr>
2017-09-11 10:06:43 +0200

    HTML et CSS

---
+++ ProjetHTML/cursus.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Projet personnel</title>
+
+</head>
+
+<body>
+
+	<div class="global">
+
+		<header class="top_page">
+			
+			<!-- Navigation de la page -->
+			<nav class="nav navbar-inverse">
+				
+				<ul class="nav nav-tabs">
+
+					<li class="presentation"><a href="main_page.html">Accueil</a></li>
+
+					<li class="presentation" class="active"><a href="moi.php">Qui suis-je ?</a></li>
+
+					<li class="presentation"><a href="cursus.php">Cursus scolaire 	</a></li>
+
+					<li class="presentation"><a href="experience.php">Expérience pro</a></li>
+
+					<li class="presentation"><a href="competences.php">Compétences</a></li>
+
+					<li class="presentation"><a href="stages.php">Stages</a></li>
+
+					<li class="presentation"><a href="#">Divers</a></li>
+
+				</ul>
+
+			</nav>
+
+		</header>
+
+		<div class="main_text">
+
+			<h1><u>Mon cursus scolaire</u></h1>
+
+			<br>
+			
+			<p class="moi">
+
+				En 2013, j'ai obtenue ma licence professionnelle Admnistration Systèmes et Réseaux, dans l'Université Paul Verlaine de Metz. <br>
+				En 2012, j'ai obtenu mon Diplôme Universitaire de Technologie en Informatique, toujours à l'Université Paul Verlaine de Metz.<br>
+				En 2009, J'ai obtenu mon baccalauréat scientifique au lycée Jean-Victor Poncelet de Saint-Avold, en ayant choisi comme spécialité "Physique-chimie".<br>
+
+			</p>
+
+		</div>
+
+	</div>
+
+	</body>
+
+</html>
---
+++ ProjetHTML/moi.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Projet personnel</title>
+
+</head>
+
+<body>
+
+	<div class="global">
+
+		<header class="top_page">
+			
+			<!-- Navigation de la page -->
+			<nav class="nav navbar-inverse">
+				
+				<ul class="nav nav-tabs">
+
+					<li class="presentation"><a href="main_page.html">Accueil</a></li>
+
+					<li class="presentation" class="active"><a href="moi.php">Qui suis-je ?</a></li>
+
+					<li class="presentation"><a href="cursus.php">Cursus scolaire 	</a></li>
+
+					<li class="presentation"><a href="experience.php">Expérience pro</a></li>
+
+					<li class="presentation"><a href="competences.php">Compétences</a></li>
+
+					<li class="presentation"><a href="stages.php">Stages</a></li>
+
+					<li class="presentation"><a href="#">Divers</a></li>
+
+				</ul>
+
+			</nav>
+
+		</header>
+
+		<div class="main_text">
+
+			<h1><u>Qui suis-je ?</u></h1>
+
+			<p class="moi">
+
+				Je me présente, je me prénomme LAUER Mathieu, 27 ans, à la recherche d'un emploi dans le domaine de l'informatique !<br><br>
+
+				Actuellement, j'habite à une adresse, que je ne souhaite pas divulguer immédiatemment, mais plûtot dans un futur proche !<br><br>
+
+				Pour m'envoyer des mails, voici mon adresse mail : <u>lauer.mathieu@orange.fr</u><br><br>
+
+				Mon numéro de téléphone portable arrivera très certainement à la même vitesse que mon adresse postale plus haut.<br><br>
+
+			</p>
+
+		</div>
+
+	</div>
+
+	</body>
+
+</html>
